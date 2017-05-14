6f3a21a88cb40e96de46140eff25943ad04112de
Matúš Mrázik <matus.mrazik2@gmail.com>
2017-05-14 19:13:23 +0200

    logging

---
+++ SEM03/SEM03/Logging/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace SEM03.Logging
+{
+    public static class Logger
+    {
+        public static string FileName { get; } = Process.GetCurrentProcess().ProcessName + ".log";
+
+        public static void LogInfo(string logStr, bool newLine = true)
+        {
+            Log("INFO", logStr, newLine);
+        }
+
+        public static void LogWarning(string logStr, bool newLine = true)
+        {
+            Log("WARNING", logStr, newLine);
+        }
+
+        public static void LogError(string logStr, bool newLine = true)
+        {
+            Log("ERROR", logStr, newLine);
+        }
+
+        private static void Log(string logType, string logMessage, bool newLine)
+        {
+#if DEBUG
+            File.AppendAllText(FileName, $@"{DateTime.Now:G} {logType}: {logMessage}" + (newLine ? Environment.NewLine : ""));
+#endif
+        }
+    }
+}
